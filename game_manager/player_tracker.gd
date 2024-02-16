4776de7f4c2f31a18d1264d20a09b08857ce8595
rose <neirdaqatyr@gmail.com>
2024-02-17 00:22:24 +0100

    tracker v1.0 done

---
+++ game_manager/player_tracker.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+signal change_player
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func round_end_detected():
+	change_player.emit()
