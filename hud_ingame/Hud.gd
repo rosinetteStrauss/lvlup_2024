c0315c822aad35782dea01254612e9f51ef27585
Nicolas Wirth <nicolas.wirth7@gmail.com>
2024-02-17 13:00:03 +0100

    Add todo to remember to add timeout managment

--- hud_ingame/Hud.gd
+++ hud_ingame/Hud.gd
@@ -2,13 +2,12 @@ extends CanvasLayer
 
 var countdown_timeout = 10
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$game_timer.set_wait_time(countdown_timeout)
 	$display_timer.set_text("Timer: " + str(countdown_timeout))
-	pass # Replace with function body.
 
 func _on_game_timer_timeout():
+	# TODO: launch new round
 	pass
 
 func _on_second_timer_timeout():
