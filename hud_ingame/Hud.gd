243c99901732e265d828d56c96b2390126e59b9d
Nicolas Wirth <nicolas.wirth7@gmail.com>
2024-02-17 17:13:38 +0100

    Added event trap

--- hud_ingame/Hud.gd
+++ hud_ingame/Hud.gd
@@ -1,19 +1,24 @@
 extends CanvasLayer
 
-var countdown_timeout = 10
+var countdown_timeout = -1
 
 func _ready():
-	$game_timer.set_wait_time(countdown_timeout)
+	countdown_timeout = $game_timer.get_wait_time()
 	$display_timer.set_text("Timer: " + str(countdown_timeout))
 
 func _on_game_timer_timeout():
 	# TODO: launch new round
-	pass
+	countdown_timeout = $game_timer.get_wait_time()
+	update_display_timer()
 
 func _on_second_timer_timeout():
 	countdown_timeout -= 1
+	if countdown_timeout == 0:
+		pass
 	update_display_timer()
 
 func update_display_timer():
+	$jec_bar.value +=1
+	$jin_bar.value +=1
 	$display_timer.set_text("Timer: " + str(countdown_timeout))
 
