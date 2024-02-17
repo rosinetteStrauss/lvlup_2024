2c19674a682f3824a4dd563f6abb1d58b4135745
rose <neirdaqatyr@gmail.com>
2024-02-17 06:07:56 +0100

    moving the player works. It is ugly as hell but did not find a clean way to use godot tools. Implemented a small gravity system than can theoretically be modified if we go to non-euclidian spaces

--- input_manager/input_manager.gd
+++ input_manager/input_manager.gd
@@ -4,14 +4,14 @@ extends Node2D
 #TODO : connect to existing signals (players move, trap landing and activation. Which player is active is managed here)
 
 enum player_name {JEC, JIN}
-enum input_move {LEFT = 0, RIGHT = 1, UP = 2, DOWN = 3, JUMP = 4}
+enum input_move {LEFT = 0, RIGHT = 1, JUMP = 2}
 
-signal move_jec(left, right, up, down, jump)
+signal move_jec(left, right, jump)
 signal land_trap_jec()
 signal activate_shared_jec(trap_id)
 signal activate_event_jec(trap_id)
 signal activate_own_jec(trap_id)
-signal move_jin(left, right, up, down, jump)
+signal move_jin(left, right, jump)
 signal land_trap_jin()
 signal activate_shared_jin(trap_id)
 signal activate_event_jin(trap_id)
@@ -25,16 +25,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var move_input = [0,0,0,0,0]	#left, right, up, down, jump : --> boolean, 1 if trigger
+	var move_input = [0,0,0]	#left, right, jump : --> boolean, 1 if trigger
 	if activ_player == player_name.JEC:
 		if Input.is_action_pressed("jec_left"):
 			move_input[input_move.LEFT] = 1
 		if Input.is_action_pressed("jec_right"):
 			move_input[input_move.RIGHT] = 1
-		if Input.is_action_pressed("jec_up"):
-			move_input[input_move.UP] = 1
-		if Input.is_action_pressed("jec_down"):
-			move_input[input_move.DOWN] = 1
 		if Input.is_action_pressed("jec_jump"):
 			move_input[input_move.JUMP] = 1
 		if Input.is_action_pressed("jec_land_trap"):
@@ -46,16 +42,12 @@ func _process(delta):
 			emit_signal("activate_event_jec", 0)
 		if Input.is_action_pressed("jec_own_trap_activation"):
 			emit_signal("activate_own_jec", 0)
-		emit_signal("move_jec", move_input[0], move_input[1], move_input[2], move_input[3], move_input[4])
+		emit_signal("move_jec", move_input[0], move_input[1], move_input[2])
 	else:
 		if Input.is_action_pressed("jin_left"):
 			move_input[input_move.LEFT] = 1
 		if Input.is_action_pressed("jin_right"):
 			move_input[input_move.RIGHT] = 1
-		if Input.is_action_pressed("jin_up"):
-			move_input[input_move.UP] = 1
-		if Input.is_action_pressed("jin_down"):
-			move_input[input_move.DOWN] = 1
 		if Input.is_action_pressed("jin_jump"):
 			move_input[input_move.JUMP] = 1
 		if Input.is_action_pressed("jin_land_trap"):
@@ -66,7 +58,7 @@ func _process(delta):
 			emit_signal("activate_event_jin", 0)
 		if Input.is_action_pressed("jin_own_trap_activation"):
 			emit_signal("activate_own_jin", 0)
-		emit_signal("move_jin", move_input[0], move_input[1], move_input[2], move_input[3], move_input[4])
+		emit_signal("move_jin", move_input[0], move_input[1], move_input[2])
 
 #TODO : obective completion and destruction are managed with input? If so add some code here
 #			Can be a simple timer upon being in the zone
--- player_manager/player_manager.gd
+++ player_manager/player_manager.gd
@@ -1,11 +1,49 @@
-extends Node2D
+extends CharacterBody2D
 
+@export var active_at_start = true
+var currently_active
+var current_velocity
+var current_gravity
+@export var speed = 20
+@export var gravity = 1
+@export var friction = 0.9
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	current_velocity = Vector2.ZERO
+	current_gravity = gravity
+	currently_active = active_at_start
+	manage_visibility()
 
+func manage_visibility():
+	if currently_active:
+		show()
+		$CollisionShape2D.disabled = false
+	else:
+		hide()
+		$CollisionShape2D.disabled = true
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	position += current_velocity
+	current_velocity.x = (current_velocity.x * (1-friction))
+	current_velocity.y += current_gravity
+	if is_on_floor():
+		current_gravity = 0
+	else:
+		current_gravity = gravity
+	move_and_slide()
+	
+
+func move(left, right, jump):
+	if left == 1:
+		current_velocity.x -= speed
+	if right == 1:
+		current_velocity.x += speed
+	if jump == 1 and is_on_floor():
+		current_velocity.y -= speed
+
+func _on_input_manager_move_jec(left, right, jump):
+	move(left, right, jump)
+
+func _on_input_manager_move_jin(left, right, jump):
+	move(left, right, jump)
