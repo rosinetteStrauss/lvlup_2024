0d92dced36129ce92c08c462a951e1919b094467
rose <neirdaqatyr@gmail.com>
2024-02-18 08:27:08 +0100

    objectives pop once at the stage start and not at each round. Both player can trigger them at the moment tho

--- ObjectiveZoneManager.gd
+++ ObjectiveZoneManager.gd
@@ -36,35 +36,38 @@ func reset_zone():
 	deploy_zone()
 
 func deploy_zone():
-	for i in range(nb_zone):
-		var line = randi_range(0,2)
-		var crt_zone
-		var x_coor
-		var y_coor
-		var x_lim_left
-		var x_lim_right
-		if line==0:	#bottom
-			x_lim_left = ground_bottom_left_lim + spawn_offset_x
-			x_lim_right = ground_bottom_right_lim - spawn_offset_x
-			y_coor = ground_bottom_y - spawn_offset_y
-		elif line == 1:	#middle
-			x_lim_left = ground_middle_left_lim + spawn_offset_x
-			x_lim_right = ground_middle_right_lim - spawn_offset_x
-			y_coor = ground_middle_y - spawn_offset_y
-		else:	#top
-			x_lim_left = ground_top_left_lim + spawn_offset_x
-			x_lim_right = ground_top_right_lim - spawn_offset_x
-			y_coor = ground_top_y - spawn_offset_y
-		x_coor = randi_range(x_lim_left, x_lim_right)	
-		$ObjectiveZone/CollisionShape2D.disabled = false
-		if player_is_eco:
-			$ObjectiveZone/Sprite2D.texture = load("res://sprites/zone_eco1_light.png")
-		else:
-			$ObjectiveZone/Sprite2D.texture = load("res://sprites/zone_indu1_light.png")
-		crt_zone = objective_zone_template.duplicate()
-		crt_zone.position = Vector2(x_coor, y_coor)
-		get_tree().root.add_child.call_deferred(crt_zone)
-		crt_zone.visible = true
+	player_is_eco = true
+	for j in range(2):
+		for i in range(nb_zone):
+			var line = randi_range(0,2)
+			var crt_zone
+			var x_coor
+			var y_coor
+			var x_lim_left
+			var x_lim_right
+			if line==0:	#bottom
+				x_lim_left = ground_bottom_left_lim + spawn_offset_x
+				x_lim_right = ground_bottom_right_lim - spawn_offset_x
+				y_coor = ground_bottom_y - spawn_offset_y
+			elif line == 1:	#middle
+				x_lim_left = ground_middle_left_lim + spawn_offset_x
+				x_lim_right = ground_middle_right_lim - spawn_offset_x
+				y_coor = ground_middle_y - spawn_offset_y
+			else:	#top
+				x_lim_left = ground_top_left_lim + spawn_offset_x
+				x_lim_right = ground_top_right_lim - spawn_offset_x
+				y_coor = ground_top_y - spawn_offset_y
+			x_coor = randi_range(x_lim_left, x_lim_right)	
+			$ObjectiveZone/CollisionShape2D.disabled = false
+			if player_is_eco:
+				$ObjectiveZone/Sprite2D.texture = load("res://sprites/zone_eco1_light.png")
+			else:
+				$ObjectiveZone/Sprite2D.texture = load("res://sprites/zone_indu1_light.png")
+			crt_zone = objective_zone_template.duplicate()
+			crt_zone.position = Vector2(x_coor, y_coor)
+			get_tree().root.add_child.call_deferred(crt_zone)
+			crt_zone.visible = true
+		player_is_eco = !player_is_eco
 
 func is_complete(zone):
 	if player_is_eco:
@@ -83,3 +86,9 @@ func _on_jec_objective_complete(zone):
 
 func _on_jin_objective_complete(zone):
 	is_complete(zone)
+
+func swap_active_player():
+	player_is_eco = !player_is_eco
+
+func _on_game_manager_new_round_signal():
+	swap_active_player()
